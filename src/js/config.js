2deaa71ee74bc6715073c32e34b707bf5113d9af
atari-monk <atari.monk1@gmail.com>
2023-02-21 21:07:44 +0100

    add page data

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const PROD_MODE = true;
-export const LOCAL = "http://127.0.0.1:5500/src/json/";
+export const LOCAL = "http://127.0.0.1:5501/src/json/";
 export const PROD = "https://atari-monk.github.io/js-notes/src/json/";
 export const DEBUG = !PROD_MODE;
