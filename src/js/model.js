e63c55381ec9d77a1301b6f811987328dda481e0
atari-monk <atari.monk1@gmail.com>
2023-03-05 19:01:48 +0100

    debug off

--- src/js/model.js
+++ src/js/model.js
@@ -1,18 +1,13 @@
 import { CONNECTION } from "./config.js";
 
 class Model {
-  #host;
-
-  constructor() {
-    this.#host = CONNECTION;
-  }
 
   async getPage(page) {
     return await this.loadJson(page);
   }
 
   async loadJson(page) {
-    return await fetch(`${this.#host}${page}.json`)
+    return await fetch(`${CONNECTION}${page}.json`)
       .then((res) => res.json());
   }
 }
