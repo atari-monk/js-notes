4705689568d35d02c2112b3a847ad1d7ec1f5de1
atari-monk <atari.monk1@gmail.com>
2023-03-08 01:28:18 +0100

    debug off

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const CONNECTION = "https://atari-monk.github.io/js-notes-json/src/json/";
-export const DEBUG = true;
+export const DEBUG = false;
 const RENDER_METHOD = Object.freeze({
   Generate: Symbol("generate"),
   Template: Symbol("template")
