91c0f60bd53683c2700e7418ed56c2521257f533
atari-monk <atari.monk1@gmail.com>
2023-03-10 21:53:25 +0100

    fix prop name

--- src/js/view/navView.js
+++ src/js/view/navView.js
@@ -7,7 +7,7 @@ export class NavView extends View {
     return this._data
       .map(
         (item) => `<li class="list-point horizontal">
-          <a class='nav-link' href="#${item.text.toLowerCase()}">${item.text}</a>
+          <a class='nav-link' href="#${item.title.toLowerCase()}">${item.title}</a>
           </li>`
       )
       .join("");
