02a441bf5be16fa4d8674f6212cc983094f04a17
atari-monk <atari.monk1@gmail.com>
2023-03-10 20:20:26 +0100

    rm enum

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,2 @@
 export const CONNECTION = "https://atari-monk.github.io/js-notes-json/src/json/";
 export const DEBUG = true;
-const RENDER_METHOD = Object.freeze({
-  Generate: Symbol("generate"),
-  Template: Symbol("template")
-});
-export const RENDER = RENDER_METHOD.Generate;
