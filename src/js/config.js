6fbd540379622c53fce532134eaa062ed0e4bab4
atari-monk <atari.monk1@gmail.com>
2023-03-13 23:45:04 +0100

    upd data path

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
-export const CONNECTION = "https://atari-monk.github.io/js-notes-json/src/json/";
+export const CONNECTION = "https://atari-monk.github.io/js-notes-json/src/json/v1/";
 export const DEBUG = true;
